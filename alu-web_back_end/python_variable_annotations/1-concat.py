efd961aaa4fd76a6b18404944dcf539e67a4b6fa
ASH-INA <156346140+ASH-INA@users.noreply.github.com>
2025-09-26 11:22:37 +0300

    Add files via upload

---
+++ alu-web_back_end/python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Args:
+        str1 (str): The first string to concatenate
+        str2 (str): The second string to concatenate
+
+    Returns:
+        str: The concatenated string of str1 and str2
+
+    Example:
+        >>> concat("Hello", "World")
+        'HelloWorld'
+    """
+    return str1 + str2
